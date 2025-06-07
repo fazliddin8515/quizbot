c4cae79046e7b9d88454d6fe39f13873436d528c
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-06-08 01:42:32 +0500

    Basic bot setup with start command with simple code

--- quizbot/handlers/users/start.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, types
-from aiogram.filters import CommandStart
-
-router = Router()
-
-@router.message(CommandStart())
-async def start_handler(message: types.Message) -> None:
-    await message.answer("Salom")
--- quizbot/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router
-from handlers.users import start
-
-
-def setup_routers() -> Router:
-    router = Router()
-    router.include_router(start.router)
-    return router
